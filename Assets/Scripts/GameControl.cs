e6b880c654547a05174702d78d5dfa3b7f18460a
GustavoSelhorstMarconi <gustavo.selmarconi@gmail.com>
2025-07-28 15:24:17 -0300

    chore(Level): Added level 16.

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mobileUI;
+    
+    private void Awake()
+    {
+        HandlePlatform();
+    }
+
+    private void HandlePlatform()
+    {
+#if UNITY_ANDROID
+        mobileUI.SetActive(true);
+#else
+        mobileUI.SetActive(false);
+#endif
+    }
+}
